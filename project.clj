48c56a63950a8dd72b89bdc1a13cf723062db1ec
Nathan Milford <nathan@milford.io>
2012-08-22 23:03:08 -0400

    yay databases and web sites

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject crybaby "0.0.1-SNAPSHOT"
   :description "crybaby events dashboard"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [noir "1.2.2"]
+                 [org.clojure/java.jdbc "0.2.3"]
+                 [org.xerial/sqlite-jdbc "3.7.2"]]
   :url "https://github.com/nmilford/crybaby"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"
@@ -9,4 +12,5 @@
   :uberjar-name "crybaby-standalone.jar"
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :omit-source true
-  :jar-exclusions [#"(?:^|/).git/"])
+  :jar-exclusions [#"(?:^|/).git/"]
+  :main crybaby.core)
